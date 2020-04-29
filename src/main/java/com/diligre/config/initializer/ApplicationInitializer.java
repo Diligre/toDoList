21c7696541d84187e33b4514ecf4cf0f7373d89b
Diligre <diligre124121@gmail.com>
2020-04-30 01:45:54 +0300

    m

--- src/main/java/com/diligre/config/initializer/ApplicationInitializer.java
+++ src/main/java/com/diligre/config/initializer/ApplicationInitializer.java
@@ -1,8 +1,6 @@
 package com.diligre.config.initializer;
 
 import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.XmlWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -14,16 +12,11 @@ public class ApplicationInitializer implements WebApplicationInitializer {
 
     @Override
     public void onStartup(ServletContext container) {
-        AnnotationConfigWebApplicationContext context
-                = new AnnotationConfigWebApplicationContext();
-        context.setConfigLocation("com.diligre");
-
-        container.addListener(new ContextLoaderListener(context));
-
-        ServletRegistration.Dynamic dispatcher = container
-                .addServlet("dispatcher", new DispatcherServlet(context));
-
+        XmlWebApplicationContext appContext = new XmlWebApplicationContext();
+        appContext.setConfigLocation("/WEB-INF/dispatcher-servlet.xml");
+        ServletRegistration.Dynamic dispatcher =
+                container.addServlet("dispatcher", new DispatcherServlet(appContext));
         dispatcher.setLoadOnStartup(1);
-        dispatcher.addMapping("/");
+        dispatcher.addMapping("/api/*");
     }
 }
